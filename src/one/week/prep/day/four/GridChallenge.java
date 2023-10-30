d49cf5eab51c6c510e8183e46572d809156e18b4
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-10-30 18:17:39 -0300

    fix: fixed duplicated main declaration

--- src/one/week/prep/day/four/GridChallenge.java
+++ src/one/week/prep/day/four/GridChallenge.java
@@ -38,8 +38,7 @@ class GridChallengeResult {
     }
 }
 public class GridChallenge {
-    public static void main(String[] args) {
-        public static void main (String[]args) throws IOException {
+    public static void main(String[] args) throws IOException {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
